40df7e7d9700705a10e33073b41ce6dee1a4feb4
GaryTang <garytang412@gmail.com>
2023-06-25 14:47:24 -0500

    uninstall test accounts app.

--- api/accounts/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-
-# Create your views here.
-def testView(req):
-    return JsonResponse(data={"hello":"world!"})
--- api/todo/settings.py
+++ api/todo/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders', # django cors app
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
